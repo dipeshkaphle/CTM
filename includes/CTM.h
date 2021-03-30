837637ffe12d07e061158d628f094c0829c82226
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2021-03-30 11:50:17 +0545

    separated out concepts usage

--- includes/CTM.h
+++ includes/CTM.h
@@ -1,26 +1,6 @@
 #pragma once
 
-#include <cassert>
-#include <cmath>
-#include <concepts>
-#include <numeric>
-#include <stdexcept>
-#include <type_traits>
-
-// ===============================================================
-// ===============================================================
-// ===============================================================
-//
-// Concepts used
-//
-template <typename T> concept Integral = std::integral<T>;
-
-template <typename T> concept UnsignedIntegral = std::unsigned_integral<T>;
-
-template <typename T> concept FloatingPoint = std::floating_point<T>;
-
-template <typename T>
-concept Number = std::integral<T> || std::floating_point<T>;
+#include "concepts.h"
 //
 // ===============================================================
 // ===============================================================
@@ -48,14 +28,17 @@ template <Integral T> constexpr T gcd(const T x, const T y) noexcept {
   return gcd(y, x % y);
 }
 
+// is_even
 template <Integral T> constexpr bool is_even(const T x) noexcept {
   return x % 2 == 0;
 }
 
+// is_odd
 template <Integral T> constexpr bool is_odd(const T x) noexcept {
   return x % 2 == 1;
 }
 
+// is_inf for doubles
 template <FloatingPoint T> constexpr bool is_inf(const T x) noexcept {
   if (std::is_constant_evaluated())
     return (x == T(INFINITY)) || (x == T(-INFINITY));
@@ -63,6 +46,7 @@ template <FloatingPoint T> constexpr bool is_inf(const T x) noexcept {
     return std::isinf(x);
 }
 
+// is_nan for doubles
 template <FloatingPoint T> constexpr bool is_nan(const T x) noexcept {
   // https://stackoverflow.com/questions/570669/checking-if-a-double-or-float-is-nan-in-c
   if (std::is_constant_evaluated()) {
@@ -70,35 +54,34 @@ template <FloatingPoint T> constexpr bool is_nan(const T x) noexcept {
   } else
     return std::isnan(x);
 }
-//
-//
-//
+
 // abs
 template <FloatingPoint T> constexpr T abs(const T n) {
   assert(!CTM::is_inf(n));
   assert(!CTM::is_nan(n));
   return (n == T(0) ? T(0) : (n < T(0) ? -n : n));
 }
-
+// abs for integral types
 template <Integral T> constexpr T abs(const T n) noexcept {
   return (n == T(0) ? T(0) : (n < T(0) ? -n : n));
 }
 
+// ceil for flaoting types
 template <FloatingPoint T> constexpr long long ceil(const T x) {
   assert(!CTM::is_inf(x));
   assert(!CTM::is_nan(x));
   return x == ((long long)x) ? (long long)x : T((long long)x + 1);
 }
-
+// ceil for integral types
 template <Integral T> constexpr T ceil(const T x) noexcept { return x; }
 
-// floor
-template <Integral T> constexpr T floor(const T x) noexcept { return x; }
-
+// floor for floating types
 template <FloatingPoint T> constexpr long long floor(const T x) {
   assert(!CTM::is_inf(x));
   assert(!CTM::is_nan(x));
   return x == ((long long)x) ? (long long)x : T((long long)x);
 }
+// floor for integral types
+template <Integral T> constexpr T floor(const T x) noexcept { return x; }
 
 } // namespace CTM
---
+++ includes/concepts.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cassert>
+#include <cmath>
+#include <concepts>
+#include <numeric>
+#include <stdexcept>
+#include <type_traits>
+
+// ===============================================================
+// ===============================================================
+// ===============================================================
+//
+// Concepts used
+//
+template <typename T> concept Integral = std::integral<T>;
+
+template <typename T> concept UnsignedIntegral = std::unsigned_integral<T>;
+
+template <typename T> concept FloatingPoint = std::floating_point<T>;
+
+template <typename T>
+concept Number = std::integral<T> || std::floating_point<T>;
